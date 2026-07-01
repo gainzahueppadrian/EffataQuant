a9f9015e111bb39913546e1ace1313ff598b6295
gainzahueppadrian <246580795+gainzahueppadrian@users.noreply.github.com>
2026-07-01 19:11:06 +0000

    feat: Complete Effata HFT Engine Core Integrations (Kelly, SGX, DGMH)

--- src/core/messages.hpp
+++ src/core/messages.hpp
@@ -7,21 +7,23 @@ namespace berkshire::core {
 
 struct alignas(64) OrderMessage {
     uint64_t id;
-    uint32_t ticker_hash;
+    uint32_t ticker_hash;   // FNV-1a 32-bit hash for O(1) symbol lookup
     uint32_t quantity;
     double limit_price;
     uint8_t  broker_id;
     uint8_t  strategy_type;
-    uint8_t  action;
+    uint8_t  action;        // 0: BUY, 1: SELL
 
+    // We adjust padding to fit exactly the 32 bytes of the signature securely
+    char     padding[5];
     security::Signature signature;
 
-    char     padding[5];
-
+    // Quick constructor
     OrderMessage() = default;
     OrderMessage(uint64_t _id, uint32_t _hash, uint32_t _qty, double _price, uint8_t _broker, uint8_t _strategy, uint8_t _action)
         : id(_id), ticker_hash(_hash), quantity(_qty), limit_price(_price), broker_id(_broker), strategy_type(_strategy), action(_action) {}
 };
 
 static_assert(sizeof(OrderMessage) == 64, "OrderMessage must be exactly 64 bytes for cache line alignment");
-}
+
+} // namespace berkshire::core
